b69a04d32a58ce932370a69179f9925034e02885
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-19 01:34:25 +0530

    use chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,13 @@
+const validator = require('validator')
+
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+console.log(validator.isEmail('abhishek@example.com'))
+console.log(validator.isEmail('example.com'))
+
+console.log(validator.isURL('https://sept.io'))
+console.log(validator.isURL('https/google.com'))
