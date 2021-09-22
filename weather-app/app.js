b6b3ed5e90c8b79e96308aa31e15e15ef7c3aaf5
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-22 21:32:56 +0530

    init

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,1 @@
-console.log("starting")
-
-setTimeout(()=>{
-    console.log('2 second timer')
-}, 2000)
-
-
-setTimeout(()=>{
-    console.log('0 second timer')
-}, 0)
-
-
-console.log("stopping")
+const request = 
