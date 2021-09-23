eae0df126f354aa7648445d4f5663e72764925a8
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-23 22:20:39 +0530

    init

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,11 @@
-const request = 
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=6253441e4737e681704e1caf5d6dda37&query=37.8267,-122.4233'
+
+request(
+    { url : url},
+    (error,response)=>{
+        const data= JSON.parse(response.body)
+        console.log(data.current)
+    }
+)
