e73b7b59233c9fb7998d6d663ddbbf5f9bd00863
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-28 04:09:46 +0530

    init

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,17 @@ const app = express()
 app.get('',(req,res)=>{
     res.send('Hello express')
 })
+app.get('/help',(req,res)=>{
+    res.send('Help Page')
+})
 
+app.get('/about',(req,res)=>{
+    res.send('About Page')
+})
+
+app.get('/weather',(req,res)=>{
+    res.send('Weather Page')
+})
 // app.com
 // app.com/help 
 // app.com/about
