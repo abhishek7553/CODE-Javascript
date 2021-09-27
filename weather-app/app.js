e99d6de210991ad45df054eec087362994fd790e
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-27 20:36:41 +0530

    weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,13 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=6253441e4737e681704e1caf5d6dda37&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=6253441e4737e681704e1caf5d6dda37&query=37.8267,-122.4233&units=f'
 
 request(
-    { url : url},
+    { url : url, json:true},
     (error,response)=>{
-        const data= JSON.parse(response.body)
-        console.log(data.current)
+        const msg =response.body.current.weather_descriptions[0] + ". It is currently "+response.body.current.temperature+" degree out. There is "+response.body.current.precip+"% chance of rain."
+        console.log(msg)
+
+
     }
 )
