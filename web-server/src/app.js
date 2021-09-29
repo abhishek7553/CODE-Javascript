b32358e87ecb803a7243b244864b7eaac2fea191
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-29 22:24:13 +0530

    weather server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,49 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.get('',(req,res)=>{
-    res.send('Hello express')
-})
-app.get('/help',(req,res)=>{
-    res.send('Help Page')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views') 
+const partialsPath = path.join(__dirname, '../templates/partials') 
+
+app.set('views',viewsPath );
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
+
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ayush'
+    })
 })
 
-app.get('/about',(req,res)=>{
-    res.send('About Page')
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ayush'
+    })
 })
 
-app.get('/weather',(req,res)=>{
-    res.send('Weather Page')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'help ',
+        name: 'Ayush'
+    })
 })
-// app.com
-// app.com/help 
-// app.com/about
 
-app.listen(3000,()=>{
-    console.log("SERVER UP AND RUNNING #3000.")
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Ranchi'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
