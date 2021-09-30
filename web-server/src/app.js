5ba87a2ffd628509311bea3167a7b9561a9f6e94
abhishek7553 <53150702+abhishek7553@users.noreply.github.com>
2021-09-30 10:21:02 +0530

    initial commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,20 +4,22 @@ const hbs = require('hbs')
 
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views') 
-const partialsPath = path.join(__dirname, '../templates/partials') 
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.set('views',viewsPath );
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Home',
+        title: 'Weather',
         name: 'Ayush'
     })
 })
@@ -32,17 +34,51 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
-        title: 'help ',
+        title: 'Help',
         name: 'Ayush'
     })
 })
 
 app.get('/weather', (req, res) => {
-    res.render('weather',{
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    res.send({
         forecast: 'It is snowing',
-        location: 'Ranchi',
-        name: 'Ayush',
-        title:'Weather'
+        location: 'Philadelphia',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
     })
 })
 
